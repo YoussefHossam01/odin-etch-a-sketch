a207ab0333569ad6b09f5ee45bd7164e4b9d839d
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-10-08 21:51:24 +0300

    Create a single div with class box

---
+++ script.js
@@ -0,0 +1,6 @@
+// Select container to manipulate
+const container = document.querySelector(".container");
+
+// create the basic box Div element
+const boxDiv = document.createElement("div");
+boxDiv.className = "box";
