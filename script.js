38e613b1f546542decf2bee56faae348c749e23a
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-10-10 17:34:21 +0300

    Now we dynamically create grids

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-// Select container to manipulate
-const container = document.querySelector(".container");
-
+// Select body to manipulate
+const body = document.querySelector("body");
 
 const GRIDSIZE = 16;
 
 // Create a grid size by grid size grid dynamically.
 for (let i = 0; i < GRIDSIZE; i++){
+    // Each grid line is its own container
+    const container = document.createElement("div");
+    container.className = "container"
+    body.appendChild(container);
     for (let j = 0; j < GRIDSIZE; j++){
         // create the basic square Div element
         const squareDiv = document.createElement("div");
