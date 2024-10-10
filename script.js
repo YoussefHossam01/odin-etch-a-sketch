94a09fc59348962606365cc9b090520a2393c820
YoussefHossam01 <146332633+YoussefHossam01@users.noreply.github.com>
2024-10-10 17:45:16 +0300

    Create any grid size grid

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const body = document.querySelector("body");
 
 const GRIDSIZE = 16;
+let cotainerWidth = 604;
 
 // Create a grid size by grid size grid dynamically.
 for (let i = 0; i < GRIDSIZE; i++){
@@ -9,10 +10,13 @@ for (let i = 0; i < GRIDSIZE; i++){
     const container = document.createElement("div");
     container.className = "container"
     body.appendChild(container);
+    let containerHeight = cotainerWidth / GRIDSIZE;
+    container.style.height = `${containerHeight}px`;
     for (let j = 0; j < GRIDSIZE; j++){
         // create the basic square Div element
         const squareDiv = document.createElement("div");
         squareDiv.className = "square";
         container.appendChild(squareDiv);
     }
+    
 }
